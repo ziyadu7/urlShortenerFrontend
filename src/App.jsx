1770bf498b901da52bbe56063c46c688f4bf2375
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-06 21:17:22 +0530

    routes set in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import UrlsPage from './Pages/urlsPage'
+import Login from './Pages/login'
+import Register from './Pages/register'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+    <Routes>
+      <Route path='/' element = {<UrlsPage/>}/>
+      <Route path='/login' element = {<Login/>}/>
+      <Route path='/register' element = {<Register/>}/>
+    </Routes>
+  </Router>
   )
 }
 

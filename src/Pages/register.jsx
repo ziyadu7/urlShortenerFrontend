aca6a61ae15081865a489b3b36799e82e7ef7a6b
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-07 17:33:44 +0530

    register page loading add

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -10,15 +10,20 @@ function Register() {
   const [password,setPassword] = useState('')
   const [err,setErr] = useState('')
   const navigate = useNavigate()
+  const [loading,setLoading] = useState(false)
 
   const handleRegister = ()=>{
+    setLoading(true)
     if(username.trim().length<=0||password.trim().length<=0){
+      setLoading(false)
       setErr('Fill all the fields')
     }else{
       axiosInstance.post('/register',{username,password}).then(res=>{
+        setLoading(false)
         toast.success('Registration successfull, Please login')
         navigate('/login')
       }).catch(err=>{
+        setLoading(false)
         if(err?.response?.data?.message){
           toast.error(err?.response?.data?.message[0])
         }else if(err?.message){
@@ -29,7 +34,7 @@ function Register() {
     }
   }
   return (
-    <UserForm isLogin={false} handleSubmit={handleRegister} setPassword={setPassword} err={err} setUsername={setUsername}/>
+    <UserForm isLogin={false} handleSubmit={handleRegister} loading={loading} setPassword={setPassword} err={err} setUsername={setUsername}/>
   )
 }
 
